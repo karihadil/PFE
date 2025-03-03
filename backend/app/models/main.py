808b1f4619cb07bfd2b67fc03cb367999a7961ad
hadil <karihadil26@gmail.com>
2025-03-02 16:16:07 -1200

    model

--- backend/app/models/main.py
+++ backend/app/models/main.py
@@ -1,1 +1,18 @@
-print("Hello, World!") 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+df = pd.read_csv('C:\\Users\\DELL\\OneDrive\\Bureau\\PFE\\backend\\app\\models\\filtered_dataset.csv')
+df['url_length'] = df['url'].apply(lambda x: len(str(x)))
+print(df.head())
+def count_special_chars(url):
+    special_chars = "@-_%=&?"
+    return sum(url.count(char) for char in special_chars)
+df['special_chars_count'] = df['url'].apply(count_special_chars)
+print(df['special_chars_count'].head())
